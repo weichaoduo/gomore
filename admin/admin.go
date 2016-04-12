775c3fbfce97c1df04c1399d36f71cf33aa612e8
sven <weichaoduo@163.com>
2016-04-12 19:48:10 +0800

    add echarts

--- admin/admin.go
+++ admin/admin.go
@@ -5,6 +5,9 @@ import (
 	"gomore/global"
 	"net/http"
 	"os"
+
+	cpu "github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
 )
 
 /**
@@ -16,8 +19,20 @@ func HttpServer() {
 	http_dir := fmt.Sprintf("%s/admin/wwwroot", wd)
 	fmt.Println("Http_dir:", http_dir)
 	http.Handle("/", http.FileServer(http.Dir(http_dir)))
-
+	http.HandleFunc("/stats", statsTask)
 	go func() {
 		http.ListenAndServe(":"+global.Config.Admin.HttpPort, nil)
 	}()
+}
+
+func statsTask(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("statsTask is running...")
+	v, _ := mem.VirtualMemory()
+	// almost every return value is a struct
+	fmt.Printf("Total: %v, Free:%v, UsedPercent:%f%%\n", v.Total, v.Free, v.UsedPercent)
+	cpuf, _ := cpu.Percent(1, true)
+	fmt.Println("Percent:", cpuf)
+	str := fmt.Sprintf(`{"conns":%d,"qps":%d,"cpu_per":%v,"mem_total":"%v","mem_free":"%v" , "mem_use_per":"%f"}`, global.SumConnections, global.Qps, cpuf, v.Total, v.Free, v.UsedPercent)
+	w.Write([]byte(str))
+
 }
